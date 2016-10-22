ca42300d8384ec3895e4b4eb699bf3c13f654a13
Dimitris Kolovos <dimitris.kolovos@york.ac.uk>
2016-10-22 13:40:31 +0100

    Produce a friendly error message when we hit https://bugs.openjdk.java.net/browse/JDK-7132194

--- io.dimitris.duix/src/io/dimitris/duix/ui/OpenFileAction.java
+++ io.dimitris.duix/src/io/dimitris/duix/ui/OpenFileAction.java
@@ -7,10 +7,13 @@ import java.awt.Frame;
 import java.io.File;
 import java.io.FilenameFilter;
 
+import javax.swing.JOptionPane;
+
 @SuppressWarnings("serial")
 public class OpenFileAction extends AppAction {
 	
 	protected File file;
+	protected String lastDirectory;
 	
 	public OpenFileAction(Duix app, File file) {
 		super(app, "Open...");
@@ -23,17 +26,30 @@ public class OpenFileAction extends AppAction {
 		File pdf = null;
 		
 		if (file == null) {
-			FileDialog fileDialog = new FileDialog((Frame) null);
+			FileDialog fileDialog = new FileDialog((Frame) null, "Open", FileDialog.LOAD);
+			
 			fileDialog.setFilenameFilter(new FilenameFilter() {
 				@Override
 				public boolean accept(File dir, String name) {
 					return name.endsWith(".pdf");
 				}
 			});
+			
+			fileDialog.setDirectory(lastDirectory);
 			fileDialog.setVisible(true);
 			
-			if (fileDialog.getFiles().length > 0) {
-				pdf = fileDialog.getFiles()[0];
+			if (fileDialog.getFile() != null) {
+				File f = fileDialog.getFiles()[0];
+				
+				if (fileDialog.getDirectory() == null) {
+					// See https://bugs.openjdk.java.net/browse/JDK-7132194
+					JOptionPane.showMessageDialog(null, "Please select the file again, from its directory (not from recent files / search). \n To stop receiving this message, please update your Java installation.", "Cannot open file", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				else {
+					lastDirectory = fileDialog.getDirectory();
+					pdf = fileDialog.getFiles()[0];
+				}
 			}
 			else {
 				return;
